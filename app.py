08e026186cd1c1d71bb08184bf1c2ea1341b70ab
4NTEDEGUEM0N <solbrilhan@gmail.com>
2025-10-06 19:45:19 -0300

    origens CORS

--- app.py
+++ app.py
@@ -23,9 +23,10 @@ load_dotenv()
 # --- Configuração do Flask ---
 ALLOWED_EXTENSIONS = {'pdf'}
 app = Flask(__name__)
-# Substitua "http://localhost:5173" pelo domínio em produção.
-CORS(app, resources={r"/api/*": {"origins": ["http://localhost:5173", "http://127.0.0.1:5000"]}})
+
+CORS(app, resources={r"/api/*": {"origins": ["http://localhost:5173", "https://exatafalada.duckdns.org", "https://exatafalada.tec.br"]}})
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+
 app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024  # Limite de 50 MB para upload
 app.secret_key = os.urandom(24)  # NECESSÁRIO para usar o sistema de mensagens 'flash'
 
